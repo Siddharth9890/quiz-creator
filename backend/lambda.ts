adb1a5e678d7c89db75d605cc3382e398f251594
Siddharth9890 <siddharth.a9890@gmail.com>
2025-05-11 17:33:53 +0530

    fix: node js build using docker

--- backend/lambda.ts
+++
@@ -1,9 +0,0 @@
-import serverlessHttp from "serverless-http";
-import { app } from ".";
-import { mongoConnection } from "./mongoConnection";
-
-if (process.env.NODE === "production") {
-    console.log("comming from lambda");
-    mongoConnection(app);
-}
-export const handler = serverlessHttp(app);
